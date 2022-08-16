7be0d6f7ca3345a94ef1aac71a0973cde029700b
dessa995 <dessa995@outlook.com>
2022-08-16 17:58:16 +0200

    sa Jquery-jem

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+$(document).ready();
+
 const groceryList = [];
 const groceryName = document.querySelector(".grocery-area");
 const addButton = document.querySelector(".add-button");
+const paragraph = document.querySelector("p");
 
 //funkcije
 
@@ -16,18 +19,37 @@ function displayAllGroceriesInHtml(groceryList) {
 
 function addGrocery(addingGroceries) {
   addButton.disabled = true;
-  addButton.style.backgroundColor = "red";
+  $(addButton).animate({
+    height: 15,
+    width: 50,
+    fontSize: 8,
+  });
+  $(addButton).fadeOut(1000);
+  $(groceryName).animate({
+    width: 80,
+    height: 10,
+  });
+  $(groceryName).fadeOut(1000);
   groceryName.disabled = true;
-  groceryName.style.backgroundColor = "gray";
 
   setTimeout(function () {
     groceryList.push(addingGroceries);
     displayAllGroceriesInHtml(groceryList);
 
     addButton.disabled = false;
-    addButton.style.backgroundColor = "rgb(187, 163, 30)";
+    $(addButton).fadeIn(200);
+    $(addButton).animate({
+      height: 38,
+      width: 80,
+      fontSize: 18,
+    });
+    $(groceryName).fadeIn(200);
+    $(groceryName).animate({
+      width: 200,
+      height: 30,
+    });
+
     groceryName.disabled = false;
-    groceryName.style.backgroundColor = "black";
   }, 1000);
 }
 
